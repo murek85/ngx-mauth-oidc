85b12687319ba1ca01ff93a493fd7b5ff553a0f6
murek85 <marcinmur@gmail.com>
2019-03-07 17:30:48 +0100

    poprawki rxjs dla Ionic 3

--- projects/ngx-mauth-oidc/src/lib/oidc.service.ts
+++ projects/ngx-mauth-oidc/src/lib/oidc.service.ts
@@ -3,9 +3,11 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 // import { Subject, Observable, Subscription, of, race } from 'rxjs';
 // import { filter, take, delay, first, tap, map } from 'rxjs/operators';
 
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { Subscription } from 'rxjs/Subscription';
+// import { Observable } from 'rxjs/Observable';
+// import { Subject } from 'rxjs/Subject';
+// import { Subscription } from 'rxjs/Subscription';
+
+import { Observable, Subject, Subscription } from 'rxjs/Rx';
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
